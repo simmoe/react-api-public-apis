a71c75bd7a60610b8e37bc050e38dc0fb7965a17
Simon Moe <simmoe@gmail.com>
2019-09-20 15:25:55 +0200

    update

--- src/components/Api.js
+++ src/components/Api.js
@@ -5,8 +5,11 @@ const Api = (props) => {
     return(
         <div className="api">
             <h1>{props.api}</h1>
-            <p>{props.description}</p>
-            {props.link ? <a href={props.link}>Visit page</a> : ""}
+            <p className="desc">{props.description}</p>
+            {props.auth === "" ? <span className="auth">NO-AUTH</span> : <span className="auth">{props.auth}</span>}
+            {props.https ? <span className="https">HTTPS</span> : ""}
+            {props.cors === "yes" ? <span className="cors">CORS</span> : <span className="cors">NO-CORS</span>}
+            {props.link ? <a target="_blank" rel="noopener noreferrer"  href={props.link}>Visit page</a> : ""}
         </div>
     )
 }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,39 +1,64 @@
 import React, {useState} from 'react'
 import Api from './Api'
 
+
 const Main = (props) => {
     // state vars - check with the api docs:
     //Se https://api.publicapis.org/
 
     const [apis,setApis] = useState([])
     const [term,setTerm] = useState('')
+    const [cats, setCats] = useState([])
+    const [category, setCategory] = useState('Animals'); 
 
+
+    //Fetch categoris and save in state
+    fetch(`https://api.publicapis.org/categories`)
+        .then(response => response.json())
+        .then(data => {
+            setCats(data)
+        })
+        .catch(e => console.log('error', e));
+    
     const onChange = (event) => {
         setTerm(event.target.value)
     }
 
+    const onSelect = (event) => {
+        setCategory(event.target.value)
+        setTerm('')
+    }
+
     const handleSubmit = (event) => {
         event.preventDefault();
-        const url = `https://api.publicapis.org/entries?title=${term}`;
+        const c = encodeURIComponent(category)
+        const t = encodeURIComponent(term)
+        const url = `https://api.publicapis.org/entries?title=${t}&category=${c}`;
         console.log(url)
 
         fetch(url)
             .then(response => response.json())
             .then(data => {
-                console.log(data.entries!== null)
+                console.log(data)
                 data.entries !== null ? setApis(data.entries) : setApis ([{API:'no results'}])
             })
             .catch(e => console.log('error', e));
     }
+
     return (
         <div className="page">
             <form onSubmit={handleSubmit}>
-                <input onChange={onChange} type="text" name="" placeholder="" />
+                <select onChange={onSelect}>
+                    {cats.map((name, index)=>{
+                        return <option key={index} value={name}>{name}</option>
+                    })}
+                </select>
+                <input placeholder="write an optional searchphrase" onChange={onChange} type="text" name="" value={term} />
                 <button>Search!</button>
                 </form>
                 <section className="section">
                     {apis.map((row, rowindex) => {
-                        return (<Api key={rowindex} api={row.API} description={row.Description} link={row.Link}/>)
+                        return (<Api key={rowindex} auth={row.Auth} cors={row.Cors} https={row.HTTPS} api={row.API} description={row.Description} link={row.Link}/>)
                     })}
                 </section>
             </div>
