09a3f3451cd3ee29756e8aa3d5f4019b14b3889f
Simon Moe <simmoe@gmail.com>
2019-09-20 14:05:56 +0200

    Updates

---
+++ precache-manifest.938084cd422b4e05021d417d26d61178.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81cb45a3ac55c40215a24d7c96131a66",
+    "url": "/react-api-public-apis/index.html"
+  },
+  {
+    "revision": "7125bec8f367c929e955",
+    "url": "/react-api-public-apis/static/css/main.c023db72.chunk.css"
+  },
+  {
+    "revision": "427dc25483357e51b6dc",
+    "url": "/react-api-public-apis/static/js/2.0c0bceae.chunk.js"
+  },
+  {
+    "revision": "7125bec8f367c929e955",
+    "url": "/react-api-public-apis/static/js/main.784442da.chunk.js"
+  },
+  {
+    "revision": "61277c7513e5e70d76d5",
+    "url": "/react-api-public-apis/static/js/runtime~main.6da4abe7.js"
+  }
+]);
